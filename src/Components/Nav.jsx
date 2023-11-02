295c393e908fc42e31c921495508a69703dd55c9
yash7xm <yash.7xm@gmail.com>
2023-11-02 17:17:20 +0530

    nav added

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+import '../assets/Styles/Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+            <div className="logo">Pluto</div>
+            <div className="features">
+                <div className="menu">Menu</div>
+                <div className="profile">Profile</div>
+                <div className="cart">Cart</div>
+                <div className="country">India</div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
